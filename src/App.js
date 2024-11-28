197424875e041e29a07b3e5a6e68c2e9a066ff8b
Srikar-1729 <srikar.s1729@gmail.com>
2024-11-28 17:34:10 +0530

    Responsiveness

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react";
 import './App.css';
 import QRCode from 'qrcode'
-
+import './index.css'; 
 
 function App(){
   const [url,setURL] = useState("Enter the url");
@@ -10,7 +10,7 @@ function App(){
   useEffect(() => {
     const canvas = document.getElementById("canvas");
     canvas.style.opacity = 0.3;
-    QRCode.toCanvas(canvas, "Enter the url", { width: 425, margin:1 }, function (error) {
+    QRCode.toCanvas(canvas, "Enter the url", { width: 425,margin:1 }, function (error) {
       if (error) console.error(error);
       console.log("QR Code generated!");
     });
@@ -25,14 +25,24 @@ function App(){
     setURL(val);
   }
 
-  function generate(){
-        const canvas = document.getElementById("canvas");
-        canvas.style.opacity = 0;
-        QRCode.toCanvas(canvas, url, { width: 425, margin:1 }, function (error) {
-          if (error) console.error(error);
-          console.log("QR Code generated!");
-      });
-      canvas.style.opacity = 1;
+  function generate() {
+    const canvas = document.getElementById("canvas");
+  
+    
+    const width = window.innerWidth <= 480 ? 200 : window.innerWidth <= 768 ? 300 : 425;
+    const height = width; 
+  
+  
+    canvas.width = width;
+    canvas.height = height;
+  
+  
+    canvas.style.opacity = 0;
+    QRCode.toCanvas(canvas, url, { width, margin: 1 }, function (error) {
+      if (error) console.error(error);
+      console.log("QR Code generated!");
+    });
+    canvas.style.opacity = 1;
   }
 
   function downloadQRCode(type) {
@@ -53,16 +63,16 @@ function App(){
     
 
   return(
-    <div class="container">
-      <div class="contents">
-        <h1 class="heading">QR Code Generator</h1>
-        <input class="link-box" type="url"
+    <div className="container">
+      <div className="contents">
+        <h1 className="heading">QR Code Generator</h1>
+        <input className="link-box" type="url"
         id="url" placeholder="Enter the url" onChange={handleChange}></input>
-        <button class="btn" id="generate" onClick={generate}>Generate QR</button>
-        <canvas id = 'canvas' width = "500" height = "400" class="qr"/>
-        <div class="download">
-            <button class="btn" onClick={()=>downloadQRCode("png")}>Download as png</button>
-            <button class="btn" onClick={()=>downloadQRCode("jpg")}>Download as jpg</button>
+        <button className="btn" id="generate" onClick={generate}>Generate QR</button>
+        <canvas id = 'canvas' class="qr"/>
+        <div className="download">
+            <button className="btn" onClick={()=>downloadQRCode("png")}>Download as png</button>
+            <button className="btn" onClick={()=>downloadQRCode("jpg")}>Download as jpg</button>
         </div>
       </div>
     </div>
